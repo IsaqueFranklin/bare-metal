dea2b4a6ea113793365a6db564ebd2833e01f8b2
Isaque Franklin <isaquefrankli@gmail.com>
2024-11-15 22:24:57 -0300

    learning stuff

--- test.c
+++ test.c
@@ -4,6 +4,7 @@ int main(void) {
     printf("Hello world!");
 } 
 
+// Just trying to lean stuff, ya know.
 /* C variables are int, char, short, long, float, double, long double */
 
 /* char type is used to store ASCII letters but it can also hold small integers from -128 to 127. It takes at least 1 byte. */
